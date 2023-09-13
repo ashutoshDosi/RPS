aa62534c75fd0822e5b85a5965072edac82a4edd
ashutoshDosi <128537598+ashutoshDosi@users.noreply.github.com>
2023-09-13 19:42:41 +0530

    Final upload of all documents
    Completed the project and have thus updated the project from local Git to GitHub

---
+++ RPS.js
@@ -0,0 +1,78 @@
+    
+      let computerMove = '';
+      let result = '';
+      let score = JSON.parse(localStorage.getItem('score')) || {
+        win: 0, 
+        loss: 0, 
+        tie: 0, 
+      }
+      updateScoreElements();
+      updateResultElements();
+
+      function updateScoreElements(){
+        document.querySelector('.js-score').innerHTML = `Wins: ${score.win}. Loss: ${score.loss}. Tie: ${score.tie}`
+      }
+
+      function updateResultElements(){
+        document.querySelector('.js-result').innerHTML = `${result}`;
+      }
+
+      function getResult (playerMove){
+        const computerMove = getComputerMove();
+
+        if (playerMove==='rock'){
+          if(computerMove==='rock'){
+            result = 'tie'
+          } else if(computerMove==='paper'){
+            result = 'loss'
+          } else if(computerMove==='scissors'){
+            result = 'win'
+          }
+        } else if (playerMove==='paper'){
+          if(computerMove==='rock'){
+            result = 'win'
+          } else if(computerMove==='paper'){
+            result = 'tie'
+          } else if(computerMove==='scissors'){
+            result = 'loss'
+          }
+        } else if (playerMove==='scissors'){
+          if(computerMove==='rock'){
+            result = 'loss'
+          } else if(computerMove==='paper'){
+            result = 'win'
+          } else if(computerMove==='scissors'){
+            result = 'tie'
+          }
+        }
+
+        if (result==='win'){
+          score.win += 1;
+        }
+        else if (result==='loss'){
+          score.loss += 1;
+        }
+        else if (result==='tie'){
+          score.tie += 1;
+        }
+
+        localStorage.setItem('score', JSON.stringify(score))
+
+        updateScoreElements();
+        updateResultElements();
+        document.querySelector('.js-moves').innerHTML = `You picked ${playerMove}. Computer picked ${computerMove}`
+      }
+
+      function getComputerMove(){
+        const randomNumber = Math.random();
+
+        if(randomNumber>=0 && randomNumber<1/3){
+          computerMove = 'rock';
+        } else if(randomNumber>=1/3 && randomNumber<2/3){
+          computerMove = 'paper'
+        } else if(randomNumber>=2/3 && randomNumber<1){
+          computerMove = 'scissors';
+        }
+        return computerMove;
+      }
+      console.log(getComputerMove())
